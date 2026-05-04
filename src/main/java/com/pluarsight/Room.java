d69c564b7f54430913cfcbe548cbae74045336ea
gwamaka1 <mwamtobegwamaka@gmail.com>
2026-05-04 13:25:55 -0400

    implemented the attributes for room class and completed the constructors as well.

--- src/main/java/com/pluarsight/Room.java
+++ src/main/java/com/pluarsight/Room.java
@@ -1,4 +1,17 @@
 package com.pluarsight;
 
 public class Room {
+    private int numberOfBeds;
+    private double price;
+    private boolean occupied;
+    private boolean dirty;
+    private boolean availble;
+
+    public Room(int numberOfBeds, boolean availble, boolean dirty, boolean occupied, double price) {
+        this.numberOfBeds = numberOfBeds;
+        this.availble = availble;
+        this.dirty = dirty;
+        this.occupied = occupied;
+        this.price = price;
+    }
 }
